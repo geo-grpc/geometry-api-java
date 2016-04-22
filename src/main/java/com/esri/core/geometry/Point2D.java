1b56f6f486701d1bd4e5529b231db00cfad195a3
David Raleigh <davidraleigh@gmail.com>
2016-04-21 21:56:06 -0700

    Kahan area need triangle calculation

--- src/main/java/com/esri/core/geometry/Point2D.java
+++ src/main/java/com/esri/core/geometry/Point2D.java
@@ -481,6 +481,40 @@ public final class Point2D implements Serializable{
 	}
 
 	public double calculateTriangleArea2D(Point2D pt1, Point2D pt2) {
-		return 0.5 * Math.abs((x - pt2.x)*(pt1.y - y) - (x - pt1.x)*(pt2.y - y));
+		double a = Point2D.distance(pt1, pt2);
+		double b = Point2D.distance(pt1, this);
+		double c = Point2D.distance(pt2, this);
+		double temp;
+
+
+		if (a < b) {
+			// set a >= b
+			temp = b;
+			b = a;
+			a = temp;
+		}
+
+		if (c > a) {
+			// set a >= c
+			temp = c;
+			c = a;
+			a = temp;
+		}
+
+		if (c > b) {
+			temp = c;
+			c = b;
+			b = temp;
+		}
+
+		//		First sort a, b, c so that a ≥ b ≥ c ; this can be done at the cost of at most three comparisons.
+		//				If c-(a-b) < 0 then the data are not side-lengths of a real triangle
+		if (c - (a - b) < 0)
+			return 0.0;
+
+		//double result = 0.5 * Math.abs((x - pt2.x)*(pt1.y - y) - (x - pt1.x)*(pt2.y - y));
+		double result = Math.sqrt((a + ( b +c)) * (c - (a - b)) * (c + (a - b)) * (a + (b - c))) / 4.0;
+		return result;
+
 	}
 }
