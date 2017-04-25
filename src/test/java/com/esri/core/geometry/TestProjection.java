8f9343a914faae7343c81be9e3ffb23d041d168b
David Raleigh <davidraleigh@gmail.com>
2017-04-25 07:37:13 -0700

    working test of proj4

--- src/test/java/com/esri/core/geometry/TestProjection.java
+++ src/test/java/com/esri/core/geometry/TestProjection.java
@@ -1,58 +1,54 @@
 package com.esri.core.geometry;
 
 import org.proj4.*;
-import com.jhlabs.map.proj.Projection;
-import com.jhlabs.map.proj.ProjectionFactory;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
-import com.jhlabs.map.*;
-
-import java.awt.geom.*;
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by davidraleigh on 11/3/16.
  */
 public class TestProjection extends TestCase {
+
+    static {
+        System.loadLibrary("proj");
+    }
     private static final double PRECESISSION = 1e-3;
 
-    private final List<Projection> inversableProjections = new ArrayList<Projection>();
+//    private final List<Projection> inversableProjections = new ArrayList<Projection>();
 
     @Before
     public void setUp() {
-        for (final String projectionName : ProjectionFactory.getOrderedProjectionNames()) {
-            final Projection projection = ProjectionFactory.getNamedProjection(projectionName);
-            if (!projection.hasInverse()) {
-                System.out.println("INFO: Ignore not inverable projection: " + projection);
-                continue;
-            }
-            inversableProjections.add(projection);
-        }
-        System.out.println();
-        System.out.flush();
+//        for (final String projectionName : ProjectionFactory.getOrderedProjectionNames()) {
+//            final Projection projection = ProjectionFactory.getNamedProjection(projectionName);
+//            if (!projection.hasInverse()) {
+//                System.out.println("INFO: Ignore not inverable projection: " + projection);
+//                continue;
+//            }
+//            inversableProjections.add(projection);
+//        }
+//        System.out.println();
+//        System.out.flush();
     }
 
     @Test
     public void testProjectionAndInverseAreTheSame() throws Exception {
-        for (final Projection projection : inversableProjections) {
-            try {
-                final java.awt.geom.Point2D.Double sourceLL = new java.awt.geom.Point2D.Double(0, 1);
-                final java.awt.geom.Point2D.Double projectedXY = projection.project(sourceLL.getX(), sourceLL.getY(), new Point2D.Double());
-                final java.awt.geom.Point2D.Double sourceLL2 = projection.projectInverse(projectedXY.getX(), projectedXY.getX(), new java.awt.geom.Point2D.Double());
-
-
-                System.out.println("INFO: Test inverable projection: " + projection);
-                checkAndPrintErrorForProjection(projection, sourceLL.getX(), sourceLL2.getX());
-                checkAndPrintErrorForProjection(projection, sourceLL.getY(), sourceLL2.getY());
-            } catch (final Exception e) {
-                System.err.println("ERROR: Exception for  " + projection);
-                e.printStackTrace(System.err);
-            }
-        }
+//        for (final Projection projection : inversableProjections) {
+//            try {
+//                final java.awt.geom.Point2D.Double sourceLL = new java.awt.geom.Point2D.Double(0, 1);
+//                final java.awt.geom.Point2D.Double projectedXY = projection.project(sourceLL.getX(), sourceLL.getY(), new Point2D.Double());
+//                final java.awt.geom.Point2D.Double sourceLL2 = projection.projectInverse(projectedXY.getX(), projectedXY.getX(), new java.awt.geom.Point2D.Double());
+//
+//
+//                System.out.println("INFO: Test inverable projection: " + projection);
+//                checkAndPrintErrorForProjection(projection, sourceLL.getX(), sourceLL2.getX());
+//                checkAndPrintErrorForProjection(projection, sourceLL.getY(), sourceLL2.getY());
+//            } catch (final Exception e) {
+//                System.err.println("ERROR: Exception for  " + projection);
+//                e.printStackTrace(System.err);
+//            }
+//        }
     }
 
     @Test
@@ -64,14 +60,30 @@ public class TestProjection extends TestCase {
                 400000,  100000,   // Second coordinate
                 600000, -100000    // Third coordinate
         };
+        double[] expected = Arrays.copyOf(coordinates,coordinates.length);
         sourcePJ.transform(targetPJ, 2, coordinates, 0, 3);
+
         System.out.println(Arrays.toString(coordinates));
-    }
 
+        for (int i = 0; i < expected.length; i++) {
+            if (i == 1)
+                continue;
 
-    private void checkAndPrintErrorForProjection(final Projection projection, final double expected, final double actual) {
-        if (Math.abs(expected - actual) > PRECESISSION) {
-            System.err.println("ERROR: Expected " + expected + ", but was " + actual + " for " + projection);
+            assertTrue((Math.abs(expected[i] - coordinates[i])) > 1);
         }
+
+        targetPJ.transform(sourcePJ, 2, coordinates, 0, 3);
+        
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], coordinates[i], .001);
+        }
+
     }
+
+
+//    private void checkAndPrintErrorForProjection(final Projection projection, final double expected, final double actual) {
+//        if (Math.abs(expected - actual) > PRECESISSION) {
+//            System.err.println("ERROR: Expected " + expected + ", but was " + actual + " for " + projection);
+//        }
+//    }
 }
