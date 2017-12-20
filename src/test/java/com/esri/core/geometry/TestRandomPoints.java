e165958abec5769d1a56369de26269279173b558
David Raleigh <davidraleigh@gmail.com>
2017-12-20 17:06:31 +0100

    tests for random points

--- src/test/java/com/esri/core/geometry/TestRandomPoints.java
+++ src/test/java/com/esri/core/geometry/TestRandomPoints.java
@@ -37,7 +37,30 @@ public class TestRandomPoints extends TestCase {
     }
 
     @Test
-    public void testMultiPartPolygonCreate() {
+    public void testPolygonWithHole() {
+        String wktpolygon = "POLYGON((0 0, 0 10, 10 10, 10 0),(3 3, 7 3, 7 7, 3 7))";
+        Geometry geometry = GeometryEngine.geometryFromWkt(wktpolygon, 0, Geometry.Type.Unknown);
+        MultiPoint multiPoint = (MultiPoint)OperatorRandomPoints.local().execute(geometry, .0013, 1977, SpatialReference.create(4326), null);
 
+        String wktPolygonNoRing = "POLYGON((0 0, 0 10, 10 10, 10 0))";
+        Geometry geometryNoRing = GeometryEngine.geometryFromWkt(wktPolygonNoRing, 0, Geometry.Type.Unknown);
+        MultiPoint multiPointNoRing = (MultiPoint)OperatorRandomPoints.local().execute(geometryNoRing, 0.0013, 1977, SpatialReference.create(4326), null);
+        Geometry geom = GeometryEngine.intersect(geometry, multiPointNoRing, SpatialReference.create(4326));
+
+        assertEquals(multiPoint.getPointCount(), ((MultiPoint)geom).getPointCount());
+    }
+
+    @Test
+    public void testMultiPartPolygonCreate() {
+        String wktpolygon2 = "MULTIPOLYGON (((0 0, 0 10, 10 10, 10 0)), ((20 0, 20 10, 30 10, 30 0)))";
+        Geometry geometry2 = GeometryEngine.geometryFromWkt(wktpolygon2, 0, Geometry.Type.Unknown);
+        MultiPoint multiPoint2 = (MultiPoint)OperatorRandomPoints.local().execute(geometry2, .0013, 1977, SpatialReference.create(4326), null);
+
+        String wktPolygon = "POLYGON((0 0, 0 10, 10 10, 10 0))";
+        Geometry geometry = GeometryEngine.geometryFromWkt(wktPolygon, 0, Geometry.Type.Unknown);
+        MultiPoint multiPoint = (MultiPoint)OperatorRandomPoints.local().execute(geometry, 0.0013, 1977, SpatialReference.create(4326), null);
+
+        assertTrue(multiPoint.getPointCount() * 2 > 3179429);
+        assertTrue(multiPoint2.getPointCount() * 2 > 3179429);
     }
 }
