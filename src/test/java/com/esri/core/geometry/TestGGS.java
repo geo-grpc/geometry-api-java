0fdfdc579a7d2553a61419f7eae06b8ccbf1342c
David Raleigh <davidraleigh@gmail.com>
2019-12-23 09:52:00 -0500

    test

--- src/test/java/com/esri/core/geometry/TestGGS.java
+++ src/test/java/com/esri/core/geometry/TestGGS.java
@@ -6,10 +6,13 @@ import org.junit.Test;
 public class TestGGS extends TestCase {
     @Test
     public void testMGRSGridZone() {
+        assertEquals(1, SpatialReference.getUTMZone(-180));
+        assertEquals(2, SpatialReference.getUTMZone(-174));
+        assertEquals(1, SpatialReference.getUTMZone(180));
+        assertEquals(60, SpatialReference.getUTMZone(179));
         assertEquals("HA", MGRS.gridSquareId(-174.000001, 0.00001));
         assertEquals("JF", MGRS.gridSquareId(-173.999999, 0.00001));
         assertEquals("1C", MGRS.gridZoneCode(-180, -80));
-        assertEquals(1, SpatialReference.getUTMZone(-180));
         assertEquals("1C", MGRS.gridZoneCode(-174.01, -76.01));
         assertEquals("2D", MGRS.gridZoneCode(-174, -72));
         assertEquals("2D", MGRS.gridZoneCode(-168.000001, -64.00001));
